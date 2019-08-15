a1dfe97354434ebdaca7d4fbd39b691efaa1348e
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-15 08:43:25 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 30}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+
+
+    def is_clickbait?
+        if title == nil || title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")
+        else
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+
 end
